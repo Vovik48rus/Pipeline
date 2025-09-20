47acfea73444ba02fed1582305fbd2374e164f20
Dereviannykh Vladimir Semenovich <62838512+Vovik48rus@users.noreply.github.com>
2025-09-20 14:06:31 +0300

    add test data

--- Pipeline_p2/Pipeline_p2.srcs/sim_1/new/test.v
+++ Pipeline_p2/Pipeline_p2.srcs/sim_1/new/test.v
@@ -3,14 +3,20 @@
 
 module test;
 
+localparam DATA_SIZE = 8;
+localparam STAGE_COUNT = 16;
+
 reg clk;
 always #10 clk <= ~clk;
 
 reg reset, fifo_out_ready, valid_in;
 
+reg [DATA_SIZE-1:0] data_in;
+
 initial
 begin
     clk = 0;
+    data_in = 5;
     reset = 1;
     fifo_out_ready = 0;
     valid_in = 0;
@@ -25,15 +31,18 @@ begin
     @(posedge clk);
     @(posedge clk);
     valid_in = 0;
+    data_in = 0;
 //    repeat (12)
 //        @(posedge clk);
 //    valid_in = 1;
     repeat (3)
         @(posedge clk);
     valid_in = 1;
+    data_in = 10;
     repeat (3)
         @(posedge clk);
     valid_in = 0;
+    data_in = 0;
     repeat (12)
         @(posedge clk);
     fifo_out_ready = 1;
@@ -43,14 +52,14 @@ begin
 end
 
 top #(
-    .DATA_SIZE(8),
-    .STAGE_COUNT(16)
+    .DATA_SIZE(DATA_SIZE),
+    .STAGE_COUNT(STAGE_COUNT)
 ) top_unit (
     .clk(clk),
     .reset(reset),
     .fifo_out_ready(fifo_out_ready),
     .valid_in(valid_in),
-    .data_in(5),
+    .data_in(data_in),
     .data_out(data_out),
     .valid_out(valid_out)
 );
